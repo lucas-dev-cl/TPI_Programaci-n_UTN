64b7a1c82af5cb1d9b9a5eb5ba71c2fb84357375
lucas-dev-cl <lucas.gallardo.dev@gmail.com>
2026-06-22 03:12:44 -0300

    chore(producto): Agrego mi variable de entorno de mi contraseña de BD

--- FoodStore/src/integrado/prog2/config/ConexionDB.java
+++ FoodStore/src/integrado/prog2/config/ConexionDB.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package integrado.prog2.config;
+package foodstore.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,9 +12,9 @@ import java.sql.SQLException;
  * @author Lila
  */
 public class ConexionDB {
-   private static final String URL = "jdbc:mysql://localhost:3306/pedidos_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/pedidos_db";
     private static final String USER = "root";
-    private static final String PASS = "root"; 
+    private static final String PASS = System.getenv("PASSWORD_DB") != null ? System.getenv("PASSWORD_DB") : "root";
 
     public static Connection getConexion() throws SQLException {
         try {
